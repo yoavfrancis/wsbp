2e9888cb9575035331bf97ed7b46ee4164a61fd1
t123581321 <t123581321@6522e092-dbab-d3a6-da6f-a6b94801be1a>
2012-06-11 20:24:57 +0000

    Added main demo application for Wii Sensor Bar Positioning

---
+++ CgWii1/CgWii1/WsbpDemo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+using GameStateManagement;
+
+namespace CgWii1
+{
+    public class WsbpDemo : Game
+    {
+        #region Fields
+
+        GraphicsDeviceManager graphics;
+        ScreenManager screenManager;
+
+
+        // By preloading any assets used by UI rendering, we avoid framerate glitches
+        // when they suddenly need to be loaded in the middle of a menu transition.
+        static readonly string[] preloadAssets =
+        {
+            "gradient",
+        };
+
+        
+        #endregion
+
+        #region Initialization
+
+        /// <summary>
+        /// The main game constructor.
+        /// </summary>
+        public WsbpDemo()
+        {
+            Content.RootDirectory = "Content";
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 853;
+            graphics.PreferredBackBufferHeight = 480;
+
+            // Create the screen manager component.
+            screenManager = new ScreenManager(this);
+
+            Components.Add(screenManager);
+
+
+        }
+
+
+        /// <summary>
+        /// Loads graphics content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            foreach (string asset in preloadAssets)
+            {
+                Content.Load<object>(asset);
+            }
+
+            // Activate the first screens.
+            screenManager.AddScreen(new BackgroundScreen(), null);
+            screenManager.AddScreen(new MainMenuScreen(), null);
+        }
+
+
+        #endregion
+
+        #region Draw
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.Black);
+
+            // The real drawing happens inside the screen manager component.
+            base.Draw(gameTime);
+        }
+
+
+        #endregion
+
+    }
+}
