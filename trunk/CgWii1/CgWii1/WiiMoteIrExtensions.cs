b8145a9b2200a59f328cb63b9b250604f8bf1bfd
t123581321 <t123581321@6522e092-dbab-d3a6-da6f-a6b94801be1a>
2012-06-12 12:19:11 +0000

    Extension Methods for WiiMote

---
+++ trunk/CgWii1/CgWii1/WiiMoteIrExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WiimoteLib;
+
+namespace CgWii1
+{
+    public static class WiiMoteIrExtensions
+    {
+        public static bool FoundAny(this Wiimote wm)
+        {
+            return wm.WiimoteState.IRState.IRSensors.Count(ir => ir.Found) > 0;
+        }
+    }
+}
